fac53b7161b9f14e2bbe7cae2ec36aa8843cf388
Arthur Queiroz <143663976+ArthurLimaQz@users.noreply.github.com>
2025-05-26 18:54:42 -0300

    att. 26/05

--- PROJETO UNIFEOB _DESENVOLVIMENTO WEB/main.py
+++ PROJETO UNIFEOB _DESENVOLVIMENTO WEB/main.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request, flash
 
 #flask sqlalchemy
 
 app = Flask(__name__)
+app.secret_key = 'ARTHURQUEIROZ' #O Flask usa a secret_key para criptografar dados da sessão e garantir que ninguém consiga alterá-los do lado do cliente. Sem isso, recursos como flash, session e login não funcionam.
 
 from routes import *
 
--- PROJETO UNIFEOB _DESENVOLVIMENTO WEB/routes.py
+++ PROJETO UNIFEOB _DESENVOLVIMENTO WEB/routes.py
@@ -1,7 +1,5 @@
-from main import app
-from flask import render_template
-from flask import redirect
-from flask import request
+from main import app, render_template, redirect, request, flash
+
 
 
 @app.route("/") #O @ no python é dado como decoration, no caso ele da uma funcionalidade para a linha que está a baixo! 
@@ -17,8 +15,17 @@ def login():
 
     if nome == "arthur.queiroz" and senha == "123":
         return render_template("registro.html")
-    return redirect('/')
+    else:
+        flash('USUARIO INVALIDO')
+        return redirect('/')
+    
 
-@app.route("/registro") #O @no python é dado como decoration, no caso ele da uma funcionalidade para a linha que está a baixo! 
+@app.route('/cadastro') #O @no python é dado como decoration, no caso ele da uma funcionalidade para a linha que está a baixo! 
+def cadastro():
+    nome = request.form.get('login')
+    senha = request.form.get('password')
+    return render_template('cadastro.html')
+
+@app.route('/registro') #O @no python é dado como decoration, no caso ele da uma funcionalidade para a linha que está a baixo! 
 def registro():
-    return render_template("registro.html")
+    return render_template('registro.html')
