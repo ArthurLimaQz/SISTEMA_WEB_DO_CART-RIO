73408dbed5f4175b0c783f9a7723f51c7c1b51bd
Arthur Queiroz <143663976+ArthurLimaQz@users.noreply.github.com>
2025-06-09 20:24:26 -0300

    Update main.py

--- PROJETO UNIFEOB _DESENVOLVIMENTO WEB/main.py
+++ PROJETO UNIFEOB _DESENVOLVIMENTO WEB/main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, redirect, request, flash, session, url_for
-from routes import *
+
 
 #flask sqlalchemy
 
 app = Flask(__name__)
 app.secret_key = 'ARTHURQUEIROZ' #O Flask usa a secret_key para criptografar dados da sessão e garantir que ninguém consiga alterá-los do lado do cliente. Sem isso, recursos como flash, session e login não funcionam.
 
+from routes import *
 
 if __name__ == "__main__":
     app.run(debug=True) 
