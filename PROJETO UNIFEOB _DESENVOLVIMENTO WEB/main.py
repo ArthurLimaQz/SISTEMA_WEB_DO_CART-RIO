499bb60633d3c13fe4abce30a2f959b2630d4616
Arthur Queiroz <143663976+ArthurLimaQz@users.noreply.github.com>
2025-06-09 20:22:19 -0300

    Update main.py

--- PROJETO UNIFEOB _DESENVOLVIMENTO WEB/main.py
+++ PROJETO UNIFEOB _DESENVOLVIMENTO WEB/main.py
@@ -6,11 +6,6 @@ from routes import *
 app = Flask(__name__)
 app.secret_key = 'ARTHURQUEIROZ' #O Flask usa a secret_key para criptografar dados da sessão e garantir que ninguém consiga alterá-los do lado do cliente. Sem isso, recursos como flash, session e login não funcionam.
 
-# Pegando a URL do PostgreSQL da variável de ambiente
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
 
 if __name__ == "__main__":
     app.run(debug=True) 
